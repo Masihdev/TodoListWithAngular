00d42976a0d3d49166a94367b44c4d91d2927f75
Masihdev <masihullahmassudi85@gmail.com>
2022-12-04 22:40:24 +0100

    tasks array created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
+  tasks: string[] = [
+    'Visit Ann',
+    'Call Dad',
+    'Go to Gym',
+    'Wash the Dishs',
+    'Shop for the Party',
+  ];
 }
