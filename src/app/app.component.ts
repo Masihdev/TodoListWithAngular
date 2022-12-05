edb1a7f9678fe42918359926cd49562639f1ced3
Masihdev <masihullahmassudi85@gmail.com>
2022-12-05 21:25:42 +0100

    add and remove methods added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,25 @@ export class AppComponent {
     'Wash the Dishs',
     'Shop for the Party',
   ];
+
+  add(newTask: string) {
+    this.tasks.push(newTask);
+  }
+
+  remove(existingTask: string) {
+    let userConfirmed = confirm(
+      `Are you sure that you want to remove the following task? \n ${existingTask}`
+    );
+
+    // \n line break
+    // confirm() displays a confirmation as alert with ok and cancel buttons
+
+    if (userConfirmed) {
+      this.tasks = this.tasks.filter((task) => task != existingTask);
+    }
+  }
+
+  taskIsDone(task: string) {
+    console.log('The task: "' + task + '" is done.');
+  }
 }
